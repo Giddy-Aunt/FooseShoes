371cf2006bd1af345a094ea33c0b3a0d3fae6485
giddy-aunt <sensitive-salaryman@yandex.ru>
2017-02-06 03:49:01 +0300

    Спрайт, отступы

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ var imagemin = require("gulp-imagemin");
 var server = require("browser-sync").create();
 var run = require("run-sequence");
 var del = require("del");
+var spritesmith = require('gulp.spritesmith');
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
@@ -42,6 +43,18 @@ gulp.task("images", function() {
     imagemin.jpegtran({progressive: true})
   ]))
   .pipe(gulp.dest("build/img"));
+});
+
+gulp.task('sprite', function () {
+    var spriteData = gulp.src('img/sprite-images/*.*').pipe(spritesmith({
+        imgName: '../img/sprite.png',
+        cssName: '_sprite.scss',
+        cssFormat: 'scss',
+        padding: 10
+    }));
+
+    spriteData.img.pipe(gulp.dest('img'));
+    spriteData.css.pipe(gulp.dest('sass'));
 });
 
 gulp.task("serve", ["style"], function() {
@@ -75,5 +88,5 @@ gulp.task("clean", function() {
 });
 
 gulp.task("build", function(fn) {
-  run("clean", "copy", "style", "images", fn);
+  run("clean", "copy", "style", "images", "sprite", fn);
 });
